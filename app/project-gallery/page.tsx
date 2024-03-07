c9b01675880b6dc7844476fb090b433f3f52b7c1
Adrean Cajigas <adrean.cajigas@gmail.com>
2024-03-07 11:31:47 -0800

    feat: Add Footer to Other Pages

--- app/project-gallery/page.tsx
+++ app/project-gallery/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar"
 import GalleryHero from "../components/gallery-hero"
 import Gallery from "../components/gallery"
 import Contact from "../components/contact"
+import Footer from "../components/footer"
 
 export default function ProjectGallery() {
   return (
@@ -13,6 +14,7 @@ export default function ProjectGallery() {
         <img src="images/floorplan.png" className="absolute top-0 left-0 h-[100%] w-[100%] opacity-20 z-[-1] pointer-events-none select-none animate-background" />
       </div>
       <Contact></Contact>
+      <Footer></Footer>
     </>
   )
 }
